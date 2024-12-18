4daf9375973d0a384c2a5901375cc91a7c9c06dc
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2024-12-18 20:02:25 +0200

    Add project files.

---
+++ Hulujan_Iulia-Petruta_Lab11/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Hulujan_Iulia_Petruta_Lab11.Data;
+using Hulujan_Iulia_Petruta_Lab11.Models;
+
+
+
+
+
+namespace Hulujan_Iulia_Petruta_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
---
+++ Hulujan_Iulia-Petruta_Lab11/Data/IRestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hulujan_Iulia_Petruta_Lab11.Models;
+
+
+
+
+namespace Hulujan_Iulia_Petruta_Lab11.Data
+{
+    //internal interface IRestService
+    //{
+    //}
+
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
